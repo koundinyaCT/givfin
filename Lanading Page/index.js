2b3bfc07ef18678a7c2b9f6d3991ee0d859be82a
koundinya valmikam <kvalmikam@gmail.com>
2022-10-14 16:48:33 +0530

    givfin websites

---
+++ Lanading Page/index.js
@@ -0,0 +1,23 @@
+      //   const res = localStorage.getItem("currentShown");
+
+      //   $(".button-active").click(function (e) {
+
+      //     if (res) {
+      //     $(`.onboarding-image-and-content-${res}`).hide();
+      //     $(`.onboarding-image-and-content-${res}`).addClass("inactive");
+      //   }
+
+      //     if (e.target.value === "onboarding") {
+      //       $(".onboarding-image-and-content-onboarding").show();
+      //       localStorage.setItem("currentShown", e.target.value);
+      //     }
+      //     else if (e.target.value === "underwriting") {
+      //       $(".onboarding-image-and-content-underwriting").addClass("active");
+      //       $(".onboarding-image-and-content-underwriting").show();
+
+      //       localStorage.setItem("currentShown", e.target.value);
+      //     }
+
+      // console.log("e is",e.target.value)
+      // $(".onboarding-image-and-content-onboarding").hide();
+      //   });
